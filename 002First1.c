70dd314b20ccba81736b8a81ae0afdccca33e0a7
SushantDeshpande1 <128233334+SushantDeshpande1@users.noreply.github.com>
2024-04-20 23:25:22 +0530

    Add files via upload

---
+++ 002First1.c
@@ -0,0 +1,20 @@
+#include<stdio.h> //this is a header file inclusion 
+//stdio.h header file contains standard function declaration /
+
+//main( ) is a entry point function in C 
+//main( ) is user-defined function
+ 
+int main(   )
+{  
+    //printf( ) is used to print some data / string on terminal  
+    // "Ganesh", "A" => string
+    // 'A' , 'B' 'C' 'E' => character  
+    // \n => escape sequence 
+
+    printf("Hello world\n"); 
+
+    return 0;
+
+    // return 0 indicates successfull execution of program 
+    // any non-zero value indicates failure  
+}
